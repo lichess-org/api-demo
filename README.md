754392c6036e23829cf39bf488a5693a4b61363b
Thibault Duplessis <t@lichess.org>
2022-03-24 15:39:20 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Lichess OAuth app demo
+
+This is an example for a fully client side OAuth app that uses various APIs.
+
+## Running
+
+1. `npm install`
+1. `npm run build`
+1. `npm run serve` or any other method to serve the app on http://localhost:8000
---
+++ rollup.config.mjs
@@ -0,0 +1,35 @@
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import scss from 'rollup-plugin-scss';
+import sass from 'sass';
+import { terser } from 'rollup-plugin-terser';
+
+export default args => ({
+  input: 'src/main.ts',
+  output: {
+    file: args['config-prod'] ? 'dist/index.min.js' : 'index.js',
+    format: 'iife',
+    name: 'LichessDemo',
+    plugins: args['config-prod']
+      ? [
+          terser({
+            safari10: false,
+            output: { comments: false },
+          }),
+        ]
+      : [],
+  },
+  plugins: [
+    resolve(),
+    typescript(),
+    commonjs(),
+    scss({
+      include: ['scss/*.scss'],
+      output: args['config-prod'] ? './dist/style.min.css' : './style.css',
+      failOnError: true,
+      runtime: sass,
+      ...(args['config-prod'] ? { outputStyle: 'compressed' } : {}),
+    }),
+  ],
+});
